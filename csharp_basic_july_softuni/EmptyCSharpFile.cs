93b49afe1ff792b4eae6f9cd311d5c24311fe565
zamunda68 <zamunda68@gmail.com>
2021-07-11 16:59:25 +0300

    Initial check-in of module csharp_basic_july_softuni

---
+++ csharp_basic_july_softuni/EmptyCSharpFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace csharp_basic_july_softuni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read numbers from the console.
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            //specify the result is a-b.
+            int result = a - b;
+            //calculate the result from the entered in the console numbers.
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ csharp_basic_july_softuni/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace csharp_basic_july_softuni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+          int age = int.Parse(Console.ReadLine());
+
+            int ageAfterSevenYears = age + 7;
+
+            Console.WriteLine(ageAfterSevenYears);
+
+
+      
+        }
+    }
+}
